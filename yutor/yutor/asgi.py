972a12fc01f25804344f0700b5f919126f825043
LukasNel <lukasabrie@gmail.com>
2022-04-08 23:10:30 -0400

    Initial Commit

---
+++ yutor/yutor/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for yutor project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yutor.settings')
+
+application = get_asgi_application()
