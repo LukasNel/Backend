3e0e61087a361580a04fa095f7ceac65e8707b76
brendxn <brendon.gong@gmail.com>
2022-04-09 00:26:44 -0400

    Add some fields

--- yutor/yutorapp/models.py
+++ yutor/yutorapp/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import User, Group
-from rest_framework import serializers
 from django.conf import settings
 # Create your models here.
 
@@ -16,14 +14,29 @@ class Tutor(models.Model):
     hourly_rate = models.FloatField()
     rating = models.FloatField()
     numRatings = models.IntegerField()
-    availability = 
-    subjects = 
+    availability = models.OneToMany(
+        base_field = 
+        size = 
+    )
+    subjects = models.OneToMany(
+        base_field = models.CharField(max_length = 50),
+        size = 
+    )
+    bio = models.CharField(max_length=200)
+
+class Tutee(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    picture = models.ImageField()
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    rating = models.FloatField()
+    numRatings = models.IntegerField()
     bio = models.CharField(max_length=200)
 
 
     pub_date = models.DateTimeField('date published')
 
-class Request(models.Model):
-    Tutor = models.OneToOneField(Tutee)
-    zoom_link = models.urls()
-    time_requested = models.FloatField()
