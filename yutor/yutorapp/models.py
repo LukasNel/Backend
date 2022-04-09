9a1313fc69b06ce513683eae4fdd9aa3eb1021ae
brendxn <brendon.gong@gmail.com>
2022-04-09 00:19:40 -0400

    pushing changes to tutor class

--- yutor/yutorapp/models.py
+++ yutor/yutorapp/models.py
@@ -5,13 +5,19 @@ from django.conf import settings
 class Tutor(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
-    email = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    picture = models.ImageField()
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
     )
     hourly_rate = models.FloatField()
     rating = models.FloatField()
+    numRatings = models.IntegerField()
+    availability = 
+    subjects = 
+    bio = models.CharField(max_length=200)
+
 
     pub_date = models.DateTimeField('date published')
 
