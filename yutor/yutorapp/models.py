c8ee2b8d442c794b88dc7e69afe5a2a90ebad6cb
LukasNel <lukasabrie@gmail.com>
2022-04-08 23:29:30 -0400

    Added models

--- yutor/yutorapp/models.py
+++ yutor/yutorapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+class Tutor(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    hourly_rate = models.FloatField()
+    rating = models.FloatField()
+    
+    pub_date = models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
