630a1ca36a242b247a864e88185978e78cecddbe
whyalex <smart.alex.huang@gmail.com>
2022-04-10 05:35:14 -0400

    Settings change

--- yutor/yutor/settings.py
+++ yutor/yutor/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_c2l@+pwztv!+8iknn9s2r1mgflf5l*t82xcmu*m_hvgq(&4nl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS =  []
 
 
 # Application definition
@@ -141,3 +141,10 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',  # <-- And here
     ],
 }
+
+
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
