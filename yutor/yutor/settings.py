82e63c36393a66262724b3f43cfa721efede4fb2
whyalex <smart.alex.huang@gmail.com>
2022-04-10 05:36:21 -0400

    setting revert

--- yutor/yutor/settings.py
+++ yutor/yutor/settings.py
@@ -145,6 +145,6 @@ REST_FRAMEWORK = {
 
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+#)
